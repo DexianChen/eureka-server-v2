ecd0ea0a1fcbddd70f1936adc7feef65250f3051
DexianChen <dex2015@aliyun.com>
2019-04-21 10:08:24 +0800

    feat(): 升级到spring boot 2.0版本

---
+++ src/main/java/com/exc/prometheus/EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.exc.prometheus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //启动服务注册中心
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
